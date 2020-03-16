4524f600aa44608f8cfe31e0601b6a9380752f54
miskina <cookie20061@hotmail.fr>
2020-03-16 19:21:30 +0100

    Implementing PlanetMap to detect obstacles and testing it

---
+++ src/main/java/com/esiea/tp4A/domain/PlanetMapImpl.java
@@ -0,0 +1,36 @@
+package main.java.com.esiea.tp4A.domain;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class PlanetMapImpl implements PlanetMap {
+	private Set<Position> obstaclePositions = new HashSet<Position>();
+		
+	@Override
+	public Set<Position> obstaclePositions() {
+		return obstaclePositions;
+	}
+	
+	public void addObstacles(int x, int y) {
+		obstaclePositions.add(Position.of(x, y, Direction.NORTH));
+	}
+
+	public boolean removeObstacles(int x, int y) {
+		if(obstaclePositions.contains(Position.of(x, y, Direction.NORTH))) {
+			obstaclePositions.remove(Position.of(x, y, Direction.NORTH));
+			return true;
+		}
+
+		return false;
+
+	}
+	
+	public boolean isThereObstacles(Position position) {
+		for(Position p : obstaclePositions) {
+			if(p.getX() == position.getX() && p.getY() == position.getY()) {
+				return true;
+			}
+		}
+		return false;
+	}
+}
