e8aaad96b08ebcc0f79dbda25b2055d7d03484f6
miskina <cookie20061@hotmail.fr>
2020-03-31 21:02:27 +0200

    sandrine is experimenting

--- src/main/java/com/esiea/tp4A/domain/MarsRoverImpl.java
+++ src/main/java/com/esiea/tp4A/domain/MarsRoverImpl.java
@@ -1,19 +1,20 @@
 package main.java.com.esiea.tp4A.domain;
 
+import java.util.ArrayList;
+
 public class MarsRoverImpl implements MarsRover {
 
-    private int x;
-    private int y;
-    private Direction direction;
+    private Position position;
     private PlanetMapImpl map;
 
-    public MarsRoverImpl() {  }
+    public Position getPosition() {
+        return position;
+    }
 
-    @Override
-    public MarsRover initialize(Position pos) {
-        this.x = pos.getX();
-        this.y = pos.getY();
-        this.direction = pos.getDirection();
+
+    public MarsRover initialize(Position position) {
+        this.position = position;
+        this.map = (PlanetMapImpl) new PlanetMapImpl().initialize();
         return this;
     }
 
@@ -30,62 +31,131 @@ public class MarsRoverImpl implements MarsRover {
 
     @Override
     public Position move(String command) {
-        switch (command) {
-            case "f":
-                if (direction == Direction.NORTH)
-                    this.y += 1;
-                if (direction == Direction.SOUTH)
-                    this.y -= 1;
-                if (direction == Direction.WEST)
-                    this.x -= 1;
-                if (direction == Direction.EAST)
-                    this.x += 1;
-                break;
-            case "b":
-                if (direction == Direction.NORTH)
-                    this.y -= 1;
-                if (direction == Direction.SOUTH)
-                    this.y += 1;
-                if (direction == Direction.WEST)
-                    this.x += 1;
-                if (direction == Direction.EAST)
-                    this.x -= 1;
-                break;
-            case "l":
-                if (direction == Direction.NORTH)
-                    this.direction = Direction.WEST;
-                else if (direction == Direction.SOUTH)
-                    this.direction = Direction.EAST;
-                else if (direction == Direction.WEST)
-                    this.direction = Direction.SOUTH;
-                else
-                    this.direction = Direction.NORTH;
-                break;
-            case "r":
-                if (direction == Direction.NORTH)
-                    this.direction = Direction.EAST;
-                else if (direction == Direction.SOUTH)
-                    this.direction = Direction.WEST;
-                else if (direction == Direction.WEST)
-                    this.direction = Direction.NORTH;
-                else
-                    this.direction = Direction.SOUTH;
-                break;
-            default:
-                return null;
+        command = command.toLowerCase();
+        System.out.println(command);
+        for (int i = 0; i < command.length(); i++) {
+            System.out.println(i +" : "+ command.charAt(i));
+            if (command.charAt(i) == 'f') {
+                if (this.map != null && this.map.isThereObstacles(this.position)) {
+                    moveForward();
+                }
+            } else if (command.charAt(i) == 'b') {
+                if (this.map != null && this.map.isThereObstacles(this.position)) {
+                    moveBackward();
+                }
+            } else if (command.charAt(i) == 'l') {
+                if (this.map != null && this.map.isThereObstacles(this.position)) {
+                    rotateLeft();
+                }
+            } else if (command.charAt(i) == 'r') {
+                if (this.map != null && this.map.isThereObstacles(this.position)) {
+                    rotateRight();
+                }
+            } else {
+                return position;
+            }
         }
-        return Position.of(x,y,direction);
+        return position;
     }
 
-    public int getX() {
+    private Position moveForward() {
+        switch (this.position.getDirection()) {
+            case NORTH:
+                position = Position.of(position.getX(),change_position_plus(position.getY()), Direction.NORTH);
+                return position;
+            case SOUTH:
+                position = Position.of(position.getX(), change_position_minus(position.getY()), Direction.SOUTH);
+                return position;
+            case WEST:
+                position = Position.of(change_position_minus(position.getX()), position.getY(), Direction.WEST);
+                return position;
+            case EAST:
+                position = Position.of(change_position_plus(position.getX()), position.getY(), Direction.EAST);
+                return position;
+            default:
+                return position;
+        }
+    }
+
+    private Position moveBackward() {
+        switch (this.position.getDirection()) {
+            case NORTH:
+                position = Position.of(position.getX(), change_position_minus(position.getY()), Direction.NORTH);
+                return position;
+            case SOUTH:
+                position = Position.of(position.getX(), change_position_plus(position.getY()), Direction.SOUTH);
+                return position;
+            case WEST:
+                position = Position.of(change_position_plus(position.getX()), position.getY(), Direction.WEST);
+                return position;
+            case EAST:
+                position = Position.of(change_position_minus(position.getX()), position.getY(), Direction.EAST);
+                return position;
+            default:
+                return position;
+        }
+    }
+
+
+    private Position rotateLeft() {
+        switch (this.position.getDirection()) {
+            case NORTH:
+                position = Position.of(position.getX(), position.getY(), Direction.WEST);
+                return position;
+            case SOUTH:
+                position = Position.of(position.getX(), position.getY(), Direction.EAST);
+                return position;
+            case WEST:
+                position = Position.of(position.getX(), position.getY(), Direction.SOUTH);
+                return position;
+            case EAST:
+                position = Position.of(position.getX(), position.getY(), Direction.NORTH);
+                return position;
+            default:
+                return position;
+        }
+    }
+
+    private Position rotateRight() {
+        switch (this.position.getDirection()) {
+            case NORTH:
+                position = Position.of(position.getX(), position.getY(), Direction.EAST);
+                return position;
+            case SOUTH:
+                position = Position.of(position.getX(), position.getY(), Direction.WEST);
+                return position;
+            case WEST:
+                position = Position.of(position.getX(), position.getY(), Direction.NORTH);
+                return position;
+            case EAST:
+                position = Position.of(position.getX(), position.getY(), Direction.SOUTH);
+                return position;
+            default:
+                return position;
+        }
+    }
+
+
+    public int change_position_plus(int x) {
+        if(x < 50) {
+            x++;
+        }
+        else if(x == 50) {
+            x = -49;
+        }
         return x;
     }
 
-    public int getY() {
-        return y;
-    }
-
-    public Direction getDirection() {
-        return direction;
+    public int change_position_minus(int x) {
+        if(x > -50) {
+            x --;
+        }
+        else if(x == -50) {
+            x = 49;
+        }
+        return x;
     }
 }
+
+
+
--- src/test/java/com/esiea/tp4A/domain/MarsRoverTest.java
+++ src/test/java/com/esiea/tp4A/domain/MarsRoverTest.java
@@ -1,113 +1,115 @@
 package test.java.com.esiea.tp4A.domain;
 
 import main.java.com.esiea.tp4A.domain.*;
-import org.junit.Before;
-import org.junit.jupiter.api.Test;
 
+import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import static org.junit.Assert.*;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
 public class MarsRoverTest {
+    final MarsRover marsRover = new MarsRoverImpl().initialize(Position.of(0,0,Direction.NORTH));
+    final MarsRover marsRoverSouth = new MarsRoverImpl().initialize(Position.of(0,0,Direction.SOUTH));
+    final MarsRover marsRoverEast = new MarsRoverImpl().initialize(Position.of(0,0,Direction.EAST));
+    final MarsRover marsRoverWest = new MarsRoverImpl().initialize(Position.of(0,0,Direction.WEST));
 
-    private MarsRoverImpl marsRover = new MarsRoverImpl();
-    Grid grid = new Grid(100,100, marsRover);
+    //Grid grid = new Grid(100,100, marsRover);
+
+    @Test
+    void move_error() {
+        Position position_error = marsRover.move("");
+        assertEquals(position_error.getX(), 0);
+        assertEquals(position_error.getY(), 0);
+        assertEquals(position_error.getDirection(), Direction.NORTH);
+    }
 
     @Test
     void move_forward() {
-        marsRover.initialize(Position.of(0,0, Direction.NORTH));
-        marsRover.move("f");
-        grid.newCordo(0,1);
-        assertEquals(marsRover.getX(), 0);
-        assertEquals(marsRover.getY(), 1);
-        assertEquals(marsRover.getDirection(), Direction.NORTH);
-        assertEquals(1, grid.getY());
+        Position position_north = marsRover.move("f");
+        assertEquals(position_north.getX(), 0);
+        assertEquals(position_north.getY(), 1);
+        assertEquals(position_north.getDirection(), Direction.NORTH);
     }
 
     @Test
     void move_backward() {
-        marsRover.initialize(Position.of(0,0,Direction.NORTH));
-        marsRover.move("b");
-        assertEquals(marsRover.getX(), 0);
-        assertEquals(marsRover.getY(), -1);
-        assertEquals(marsRover.getDirection(), Direction.NORTH);
+        Position position_south = marsRover.move("b");
+        assertEquals(position_south.getX(), 0);
+        assertEquals(position_south.getY(), -1);
+        assertEquals(position_south.getDirection(), Direction.NORTH);
     }
 
     @Test
     void move_left() {
-        marsRover.initialize(Position.of(0,0,Direction.NORTH));
-        marsRover.move("l");
-        assertEquals(marsRover.getX(), 0);
-        assertEquals(marsRover.getY(), 0);
-        assertEquals(marsRover.getDirection(), Direction.WEST);
+        Position position_west = marsRover.move("l");
+        assertEquals(position_west.getX(), 0);
+        assertEquals(position_west.getY(), 0);
+        assertEquals(position_west.getDirection(), Direction.WEST);
     }
 
     @Test
     void move_right() {
-        marsRover.initialize(Position.of(0,0,Direction.NORTH));
-        marsRover.move("r");
-        assertEquals(marsRover.getX(), 0);
-        assertEquals(marsRover.getY(), 0);
-        assertEquals(marsRover.getDirection(), Direction.EAST);
+        Position position_east = marsRover.move("r");
+        assertEquals(position_east.getX(), 0);
+        assertEquals(position_east.getY(), 0);
+        assertEquals(position_east.getDirection(), Direction.EAST);
     }
 
     @Test
     void move_right_forward() {
-        marsRover.initialize(Position.of(0,0,Direction.NORTH));
-        marsRover.move("r");
-        marsRover.move("f");
-        assertEquals(marsRover.getX(), 1);
-        assertEquals(marsRover.getY(), 0);
-        assertEquals(marsRover.getDirection(), Direction.EAST);
+        Position position_east_forward = marsRover.move("rf");
+        assertEquals(position_east_forward.getX(), 1);
+        assertEquals(position_east_forward.getY(), 0);
+        assertEquals(position_east_forward.getDirection(), Direction.EAST);
     }
 
     @Test
     void move_left_forward() {
-        marsRover.initialize(Position.of(0,0,Direction.NORTH));
-        marsRover.move("l");
-        marsRover.move("f");
-        assertEquals(marsRover.getX(), -1);
-        assertEquals(marsRover.getY(), 0);
-        assertEquals(marsRover.getDirection(), Direction.WEST);
+        Position position_west_forward = marsRover.move("lf");
+        assertEquals(position_west_forward.getX(), -1);
+        assertEquals(position_west_forward.getY(), 0);
+        assertEquals(position_west_forward.getDirection(), Direction.WEST);
     }
 
+    @Test
+    void move_to_1_2_w() {
+        Position position_west_forward = marsRover.move("fflb");
+        assertEquals(position_west_forward.getX(), 1);
+        assertEquals(position_west_forward.getY(), 2);
+        assertEquals(position_west_forward.getDirection(), Direction.WEST);
+    }
 
 
     @Test
     void moving_north_when_0_50_N() {
-        marsRover.initialize(Position.of(0,50,Direction.NORTH));
-        marsRover.move("f");
-        grid.newCordo(0,-49);
-        marsRover.initialize(Position.of(grid.getX(),grid.getY(),Direction.NORTH));
-        assertEquals(-49, grid.getY());
+        final MarsRover marsRover = new MarsRoverImpl().initialize(Position.of(0,50,Direction.NORTH));
+        Position position_when_0_50_N = marsRover.move("f");
+        assertEquals(-49, position_when_0_50_N.getY());
     }
     @Test
     void moving_east_when_50_0_E() {
-        marsRover.initialize(Position.of(50,0,Direction.EAST));
-        marsRover.move("f");
-        grid.newCordo(-49,-0);
-        marsRover.initialize(Position.of(grid.getX(),grid.getY(),Direction.NORTH));
-        assertEquals(-49, grid.getX());
+        final MarsRover marsRover = new MarsRoverImpl().initialize(Position.of(50,0,Direction.EAST));
+        Position position_when_50_0_E = marsRover.move("f");
+        assertEquals(-49, position_when_50_0_E.getX());
     }
     @Test
     void moving_south_when_0_minus_50_S() {
-        marsRover.initialize(Position.of(0,-50,Direction.SOUTH));
-        marsRover.move("f");
-        grid.newCordo(0,49);
-        marsRover.initialize(Position.of(grid.getX(),grid.getY(),Direction.NORTH));
-        assertEquals(49, grid.getY());
+        final MarsRover marsRover = new MarsRoverImpl().initialize(Position.of(0,-50,Direction.SOUTH));
+        Position position_when_0_minus_50_S = marsRover.move("f");
+        assertEquals(49, position_when_0_minus_50_S.getY());
     }
     @Test
     void moving_west_when_minus_50_0_W() {
-        marsRover.initialize(Position.of(-50,0,Direction.WEST));
-        marsRover.move("f");
-        grid.newCordo(50,0);
-        marsRover.initialize(Position.of(grid.getX(),grid.getY(),Direction.NORTH));
-        assertEquals(50, grid.getX());
+        final MarsRover marsRover = new MarsRoverImpl().initialize(Position.of(50,0,Direction.WEST));
+        Position position_when_minus_50_0_W = marsRover.move("f");
+        assertEquals(49, position_when_minus_50_0_W.getX());
     }
 
     @Test
     void add_obstacles_and_check_if_there_is_obstacle() {
-        PlanetMapImpl planetMapImpl = new PlanetMapImpl();
+        PlanetMapImpl planetMapImpl = (PlanetMapImpl) new PlanetMapImpl().initialize();
         planetMapImpl.addObstacles(4, 6);
         boolean checkOb1 = planetMapImpl.isThereObstacles(Position.of(4, 6, Direction.NORTH));
         assertEquals(true, checkOb1);
@@ -116,10 +118,9 @@ public class MarsRoverTest {
         marsRover.updateMap(planetMapImpl);
     }
 
-
     @Test
     void remove_the_obstacle() {
-        PlanetMapImpl planetMapImpl = new PlanetMapImpl();
+        PlanetMapImpl planetMapImpl = (PlanetMapImpl) new PlanetMapImpl().initialize();
         planetMapImpl.addObstacles(4, 4);
         boolean obj = planetMapImpl.isThereObstacles(Position.of(4,4,Direction.NORTH));
         assertEquals(true,obj);
@@ -129,75 +130,101 @@ public class MarsRoverTest {
     }
 
     @Test
-    void move_forward_with_obstacle() {
-        PlanetMapImpl planetMapImpl = new PlanetMapImpl();
+    void move_forward_with_obstacle_north() {
+        PlanetMapImpl planetMapImpl = (PlanetMapImpl) new PlanetMapImpl().initialize();
         planetMapImpl.addObstacles(0, 1);
-        boolean checkObs = planetMapImpl.isThereObstacles(Position.of(0, 1, Direction.NORTH));
-        marsRover.initialize(Position.of(0,0,Direction.NORTH));
-        if(checkObs == true) {
-            System.out.println("Command ignored");
-        }
-        else {
-            marsRover.move("f");
-        }
-        assertEquals(marsRover.getX(), 0);
-        assertEquals(marsRover.getY(), 0);
+        marsRover.updateMap(planetMapImpl);
+        Position position_obstacles = marsRover.move("f");
+        assertEquals(position_obstacles.getX(), 0);
+        assertEquals(position_obstacles.getY(), 0);
+        assertEquals(position_obstacles.getDirection(), Direction.NORTH);
+
     }
 
     @Test
-    void move_backward_with_obstacle() {
-        PlanetMapImpl planetMapImpl = new PlanetMapImpl();
+    void move_forward_with_obstacle_south() {
+        PlanetMapImpl planetMapImpl = (PlanetMapImpl) new PlanetMapImpl().initialize();
         planetMapImpl.addObstacles(0, -1);
+        marsRover.updateMap(planetMapImpl);
+        Position position_obstacles = marsRoverSouth.move("f");
+        assertEquals(position_obstacles.getX(), 0);
+        assertEquals(position_obstacles.getY(), 0);
+        assertEquals(position_obstacles.getDirection(), Direction.SOUTH);
+    }
 
-        boolean checkObs = planetMapImpl.isThereObstacles(Position.of(0, -1, Direction.NORTH));
+    @Test
+    void move_forward_with_obstacle_east() {
+        PlanetMapImpl planetMapImpl = (PlanetMapImpl) new PlanetMapImpl().initialize();
+        planetMapImpl.addObstacles(1, 0);
+        marsRover.updateMap(planetMapImpl);
+        Position position_obstacles = marsRoverEast.move("f");
+        assertEquals(position_obstacles.getX(), 0);
+        assertEquals(position_obstacles.getY(), 0);
+        assertEquals(position_obstacles.getDirection(), Direction.EAST);
+    }
 
-        marsRover.initialize(Position.of(0,0,Direction.NORTH));
-        if(checkObs == true) {
-            System.out.println("Command ignored");
-        }
-        else {
-            marsRover.move("b");
-        }
-        assertEquals(marsRover.getX(), 0);
-        assertEquals(marsRover.getY(), 0);
+    @Test
+    void move_forward_with_obstacle_west() {
+        PlanetMapImpl planetMapImpl = (PlanetMapImpl) new PlanetMapImpl().initialize();
+        planetMapImpl.addObstacles(-1, 0);
+        marsRover.updateMap(planetMapImpl);
+        Position position_obstacles = marsRoverWest.move("f");
+        assertEquals(position_obstacles.getX(), 0);
+        assertEquals(position_obstacles.getY(), 0);
+        assertEquals(position_obstacles.getDirection(), Direction.WEST);
+    }
+
+    @Test
+    void move_backward_obstacles() {
+        PlanetMapImpl planetMapImpl = (PlanetMapImpl) new PlanetMapImpl().initialize();
+        planetMapImpl.addObstacles(0, -1);
+        marsRover.updateMap(planetMapImpl);
+        Position position_obstacles = marsRover.move("b");
+        assertEquals(position_obstacles.getX(), 0);
+        assertEquals(position_obstacles.getY(), 0);
+        assertEquals(position_obstacles.getDirection(), Direction.NORTH);
     }
 
     @Test
     void move_left_with_obstacle() {
-        PlanetMapImpl planetMapImpl = new PlanetMapImpl();
+        PlanetMapImpl planetMapImpl = (PlanetMapImpl) new PlanetMapImpl().initialize();
         planetMapImpl.addObstacles(-1, 0);
-
-        boolean checkObs = planetMapImpl.isThereObstacles(Position.of(-1, 0, Direction.NORTH));
-
-        marsRover.initialize(Position.of(0,0,Direction.NORTH));
-        if(checkObs == true) {
-            System.out.println("Command ignored");
-        }
-        else {
-            marsRover.move("l");
-            marsRover.move("f");
-        }
-        assertEquals(marsRover.getX(), 0);
-        assertEquals(marsRover.getY(), 0);
+        marsRover.updateMap(planetMapImpl);
+        Position position_obstacles = marsRover.move("lf");
+        assertEquals(position_obstacles.getX(), 0);
+        assertEquals(position_obstacles.getY(), 0);
     }
 
     @Test
     void move_right_with_obstacle() {
-        PlanetMapImpl planetMapImpl = new PlanetMapImpl();
+        PlanetMapImpl planetMapImpl = (PlanetMapImpl) new PlanetMapImpl().initialize();
         planetMapImpl.addObstacles(1, 0);
+        marsRover.updateMap(planetMapImpl);
+        Position position_obstacles = marsRover.move("rf");
+        assertEquals(position_obstacles.getX(), 0);
+        assertEquals(position_obstacles.getY(), 0);
+    }
 
-        boolean checkObs = planetMapImpl.isThereObstacles(Position.of(1, 0, Direction.NORTH));
+    @Test
+    void move_f_f_f_obstacles() {
+        PlanetMapImpl planetMapImpl = (PlanetMapImpl) new PlanetMapImpl().initialize();
+        planetMapImpl.addObstacles(0, 2);
+        marsRover.updateMap(planetMapImpl);
+        Position position_obstacles = marsRover.move("fff");
+        assertEquals(position_obstacles.getX(), 0);
+        assertEquals(1,position_obstacles.getY());
+        //assertEquals(position_obstacles.getDirection(), Direction.EAST);
+    }
 
-        marsRover.initialize(Position.of(0,0,Direction.NORTH));
-        if(checkObs == true) {
-            System.out.println("Command ignored");
-        }
-        else {
-            marsRover.move("r");
-            marsRover.move("f");
-        }
-        assertEquals(marsRover.getX(), 0);
-        assertEquals(marsRover.getY(), 0);
+    @Test
+    void move_f_f_l_b_obstacles() {
+        PlanetMapImpl planetMapImpl = (PlanetMapImpl) new PlanetMapImpl().initialize();
+        planetMapImpl.addObstacles(0, 1);
+        marsRover.updateMap(planetMapImpl);
+        Position position_obstacles = marsRover.move("fflb");
+        assertEquals(position_obstacles.getX(), 1);
+        assertEquals(position_obstacles.getY(), 0);
+        //assertEquals(position_obstacles.getDirection(), Direction.EAST);
     }
 
 }
