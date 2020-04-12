315baff9d4d0e9ba3b3c4d358b40f207fdbce01b
Adonis <apiakamfart@protonmail.com>
2020-04-12 16:51:24 +0200

    refactoring de la classe ApiImpl

--- src/api/java/com/esiea/tp4A/domain/ApiImpl.java
+++ src/api/java/com/esiea/tp4A/domain/ApiImpl.java
@@ -1,11 +1,9 @@
 package api.java.com.esiea.tp4A.domain;
-
 import main.java.com.esiea.tp4A.domain.*;
 import main.java.com.esiea.tp4A.domain.Direction;
 import main.java.com.esiea.tp4A.domain.MarsRoverImpl;
 import main.java.com.esiea.tp4A.domain.PlanetMapImpl;
 import main.java.com.esiea.tp4A.domain.Position;
-
 import java.util.*;
 
 public class ApiImpl implements Api {
@@ -36,28 +34,23 @@ public class ApiImpl implements Api {
     public Position getPosition(String playerName) {
         return findRover(playerName).getPosition();
     }
-
     @Override
     public Set<Position> getObstaclesPlayers(String playerName) {
         return null;
     }
-
     @Override
     public int getLaserRange(String playerName) {
         return laserRange;
     }
-
     @Override
     public Position movePlayer(String playerName, String command) {
         return findRover(playerName).getPosition();
     }
-
     @Override
     public void laserShoot(String playerName) {
         Position playerPosition = findRover(playerName).getPosition();
         findRover(playerName).getLaserRange();
     }
-
     @Override
     public boolean isPlayerAlive(String playerName) {
         return false;
